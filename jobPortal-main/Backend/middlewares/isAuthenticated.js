e118a9a6da904facaf47669d01548b11d764c478
bhanu pratap bhana <bhanubhana02@gmail.com>
2026-06-17 07:30:20 +0530

    Add logging for cookies and token in authentication middleware

--- jobPortal-main/Backend/middlewares/isAuthenticated.js
+++ jobPortal-main/Backend/middlewares/isAuthenticated.js
@@ -4,6 +4,9 @@ const isAuthenticated =  async (req,res,next) => {
     try {
       const token = req.cookies.token;
 
+      console.log("Cookies:", req.cookies);
+      console.log("Token:", token);
+
       if (!token) {
         return res.status(401).json({
           message: "You are not authenticated",
